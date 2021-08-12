a0902dcd0e0582e00f8033e6ca792b07a5d47d11
Sergei Maksimov <s.mksimov@gmail.com>
2021-08-13 00:16:40 +0300

    added TransactionsList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+import TransactionsList from './conponent/TransactionsList';
 
 function App() {
+
+  const [transactions, setTransactions] = useState([
+    {
+      id: 1,
+      date: '2021-08-12',
+      type: 'Еда вне дома',
+      title: 'Мороженка',
+      sum: 550,
+      debit: 'Прибыль',
+      credit: 'Карта',
+      comment: 'Какой-то комментарий'
+    },
+    {
+      id: 2,
+      date: '2021-08-12',
+      type: 'Еда вне дома',
+      title: 'Мороженка',
+      sum: 550,
+      debit: 'Прибыль',
+      credit: 'Карта',
+      comment: 'Какой-то комментарий'
+    },
+    {
+      id: 3,
+      date: '2021-08-12',
+      type: 'Еда вне дома',
+      title: 'Мороженка',
+      sum: 550,
+      debit: 'Прибыль',
+      credit: 'Карта',
+      comment: 'Какой-то комментарий'
+    }
+  ]);
+
+
   return (
-    <div className="App">
-      Hello!
+    <div className='App'>
+      <TransactionsList transactions={transactions}/>
     </div>
   );
 }
