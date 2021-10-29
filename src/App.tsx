32784947ac03ccec7748abbc3d6088eb10f4f94b
Sergei Maksimov.com <s.mksimov@gmail.com>
2021-10-29 22:42:30 +0300

    change Routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,29 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { SignUpPage } from './pages/SignUpPage/SignUpPage';
 import './App.css';
 import { LoginPage } from './pages/LoginPage';
+import { useAppSelector } from './hooks/hooks';
 
 
 export const App = () => {
+
+  const { isAuth } = useAppSelector(state => state.authSlice);
 
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Navbar />
       <div className='wrapper'>
+        {!isAuth &&
         <Switch>
-          <Route path='/signup' component={SignUpPage} />
           <Route path='/login' component={LoginPage} />
+          <Route path='/signup' component={SignUpPage} />
+        </Switch>
+        }
+        {isAuth &&
+        <Switch>
           <Route path='/' component={FinancialStatus} />
         </Switch>
+        }
       </div>
     </BrowserRouter>
   )
